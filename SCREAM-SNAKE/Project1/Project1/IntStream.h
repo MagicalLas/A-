45bd9523243f888b59c146562ba7e2d9e142d622
La <35015234+Las-Wonho@users.noreply.github.com>
2018-04-26 19:44:37 +0900

    .

---
+++ SCREAM-SNAKE/Project1/Project1/IntStream.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+#include<stdio.h>
+#include<functional>
+#include<vector>
+using namespace std;
+struct intS
+{
+	vector<int>::iterator left;
+	vector<int>::iterator right;
+	void set(vector<int>::iterator l, vector<int>::iterator r) {
+		left = l;
+		right = r;
+	}
+	void print() noexcept {
+		for (auto i = left; i != right; ++i)
+		{
+			cout << *i << ",";
+		}
+	};
+	void map(function<int(int)> f) noexcept {
+		for (auto i = left; i != right; ++i)
+		{
+			cout << f(*i) << ",";
+		}
+	};
+};
