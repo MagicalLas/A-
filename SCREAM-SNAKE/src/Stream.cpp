a93875223cddf5e867a74aa9092a858e6e750392
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-04-28 15:38:43 +0900

    스트림 생성.

--- SCREAM-SNAKE/src/Stream.cpp
+++ SCREAM-SNAKE/src/Stream.cpp
@@ -1,46 +1,36 @@
-
-#include "stdafx.h"
 #include<iostream>
 #include<stdio.h>
 #include<functional>
+#include "stdafx.h"
 #include<vector>
 
 using namespace std;
 
-template<typename f = function<int(void)>>
-class IntStream {
-public:
-	IntStream(f func) {
-		this->first_num = 1;
-		this->func = func;
+struct IntStream
+{
+	vector<int>::iterator left, right;
+	void set(vector<int> v) {
+		left = v.begin();
+		right = v.end();
 	};
-
-	IntStream map(function<int(int)>) {
-
+	void f(function<int(int)> g) {
+		for (int i = 0; left!=right; left++)
+		{
+			*left=g(*left);
+		}
 	};
-	IntStream filter(function<bool(int)>) {
+	void print() {
+		for (int i = 0; left != right; left++)
+		{
+			cout << *left << " ";
+		}
 	};
-	IntStream limit(int number) {
-
-	};
-	IntStream foreach(function<void(int)>) {
-
-	};
-	IntStream reduce(function<int(int, int)>) {
-
-	};
-	IntStream iterator(function<int(int)>) {
-	};
-
-	int do_it() {
-		int num = this->first_num;
-		num = func(num);
-		return num;
-	}
-private:
-	int first_num;
-	int function_num;
-	f func;
 };
+
 int main() {
+	vector<int> v = {1,2,3,4,5,6,7,8,9};
+	IntStream s;
+	s.set(v);
+	s.f([](int x)->int {return 4; });
+	s.print();
 }
