5afd23c97ebe3981cc9385ba911707edf08053a0
La <35015234+Las-Wonho@users.noreply.github.com>
2018-05-09 12:34:47 +0900

    간단한 파일 분할 완성.

--- SCREAM-SNAKE/Stream_Play/Stream_Play/Stream.cpp
+++ SCREAM-SNAKE/Stream_Play/Stream_Play/Stream.cpp
@@ -1,6 +1,22 @@
-#include"Stream.h"
-#include<vector>
-#include<functional>
 #include"stdafx.h"
-using namespace Stream;
-void IntStream::print() { int f; };
+#include"Stream.h"
+#include<iostream>
+void Stream::IntStream::print() noexcept{
+	for (auto i = left; i + 1 != right; ++i)
+	{
+		std::cout << *i << ", ";
+	}
+	std::cout << *(right - 1) << std::endl;
+};
+
+void Stream::IntStream::function(std::function<int(int)> f) {
+	for (auto i = left; i != right; ++i)
+	{
+		*i = f(*i);
+	}
+};
+void Stream::IntStream::set(std::vector<int> v) {
+	vector_ = v;
+	left = vector_.begin();
+	right = vector_.end();
+}
--- SCREAM-SNAKE/Stream_Play/Stream_Play/Stream.h
+++ SCREAM-SNAKE/Stream_Play/Stream_Play/Stream.h
@@ -1,15 +1,15 @@
 #pragma once
+#include<vector>
+#include<functional>
 namespace Stream {
 	class IntStream {
-	public:
-		void set(std::vector<int> v);
-		void set_iterator(std::vector<int>::iterator left_, std::vector<int>::iterator right_);
-		void print();
-		int sum();
-		IntStream iterator(std::function<int(int)> f, int initialize, int num);
 	private:
 		std::vector<int>::iterator left, right;
 		std::vector<int> vector_;
+	public:
+		void set(std::vector<int> v);
+		void set_iterator(std::vector<int>::iterator left_, std::vector<int>::iterator right_);
+		void function(std::function<int(int)> f);
+		void print()noexcept;
 	};
-
-};
+}
